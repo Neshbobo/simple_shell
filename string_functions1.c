38bce8817e582d90c3827469af275596fedc6820
Neshbobo <wambuineema27@gmail.com>
2023-12-13 03:04:10 -0800

    1

--- string_functions1.c
+++ string_functions1.c
@@ -1,87 +1,37 @@
 #include "shell.h"
 
 /**
-* _strcpy - copies a string
-* @dest: the destination
-* @src: the source
-*
-* Return: pointer to destination
+* print_prompt - Display the shell prompt
 */
-char *_strcpy(char *dest, char *src)
+void print_prompt(void)
 {
-int i = 0;
-
-if (dest == src || src == 0)
-return (dest);
-while (src[i])
-{
-dest[i] = src[i];
-i++;
-}
-dest[i] = 0;
-return (dest);
+printf("$ ");
 }
 
 /**
-* _strdup - duplicates a string
-* @str: the string to duplicate
-*
-* Return: pointer to the duplicated string
+* execute_command - Execute the given command
+* @command: The command to be executed
 */
-char *_strdup(const char *str)
+void execute_command(const char *command)
 {
-int length = 0;
-char *ret;
+pid_t pid = fork();
 
-if (str == NULL)
-return (NULL);
-while (*str++)
-length++;
-ret = malloc(sizeof(char) * (length + 1));
-if (!ret)
-return (NULL);
-for (length++; length--;)
-ret[length] = *--str;
-return (ret);
+if (pid == -1)
+{
+perror("fork");
+exit(EXIT_FAILURE);
 }
-
-/**
-*_puts - prints an input string
-*@str: the string to be printed
-*
-* Return: Nothing
-*/
-void _puts(char *str)
+else if (pid == 0)
 {
-int i = 0;
-
-if (!str)
-return;
-while (str[i] != '\0')
+if (execlp(command, command, (char *)NULL) == -1)
 {
-_putchar(str[i]);
-i++;
+perror("execlp");
+exit(EXIT_FAILURE);
 }
 }
-
-/**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
+else
 {
-static int i;
-static char buf[WRITE_BUF_SIZE];
-
-if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-{
-write(1, buf, i);
-i = 0;
+int status;
+waitpid(pid, &status, 0);
 }
-if (c != BUF_FLUSH)
-buf[i++] = c;
-return (1);
 }
